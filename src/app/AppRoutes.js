10791850ea41d23f2bcdc4f0cd5c6ca0c45c50cb
IwanHPrasetyo <Auxx28@gmail.com>
2023-01-19 20:01:11 +0700

    rooting client

--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -5,18 +5,8 @@ import Spinner from "../app/shared/Spinner";
 
 const Dashboard = lazy(() => import("./dashboard/Dashboard"));
 
-const Buttons = lazy(() => import("./basic-ui/Buttons"));
-const Dropdowns = lazy(() => import("./basic-ui/Dropdowns"));
-const Typography = lazy(() => import("./basic-ui/Typography"));
-
-const BasicElements = lazy(() => import("./form-elements/BasicElements"));
-
-const BasicTable = lazy(() => import("./tables/BasicTable"));
+const Client = lazy(() => import("./screen/client"));
 const InvoiceTable = lazy(() => import("./screen/invoice"));
-
-const Mdi = lazy(() => import("./icons/Mdi"));
-
-const ChartJs = lazy(() => import("./charts/ChartJs"));
 
 const Error404 = lazy(() => import("./error-pages/Error404"));
 const Error500 = lazy(() => import("./error-pages/Error500"));
@@ -31,21 +21,8 @@ class AppRoutes extends Component {
         <Switch>
           <Route exact path="/dashboard" component={Dashboard} />
 
-          <Route path="/basic-ui/buttons" component={Buttons} />
-          <Route path="/basic-ui/dropdowns" component={Dropdowns} />
-          <Route path="/basic-ui/typography" component={Typography} />
-
-          <Route
-            path="/form-Elements/basic-elements"
-            component={BasicElements}
-          />
-
-          <Route path="/tables/basic-table" component={BasicTable} />
+          <Route path="/tables/basic-table" component={Client} />
           <Route path="/tables/invoice-table" component={InvoiceTable} />
-
-          <Route path="/icons/mdi" component={Mdi} />
-
-          <Route path="/charts/chart-js" component={ChartJs} />
 
           <Route path="/user-pages/login-1" component={Login} />
           <Route path="/user-pages/register-1" component={Register1} />
