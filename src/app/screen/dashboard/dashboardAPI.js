bef495ea17eb2de20b340ac9d7becc2bca145fcf
IwanHPrasetyo <Auxx28@gmail.com>
2023-01-22 23:55:59 +0700

    expired client

--- src/app/screen/dashboard/dashboardAPI.js
+++ src/app/screen/dashboard/dashboardAPI.js
@@ -30,4 +30,19 @@ const getClient = () => {
   });
 };
 
-export { getSumClient, getClient };
+const getExpired = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`http://localhost:8080/api/client/getExpired`)
+      .then((result) => {
+        console.log("berhasil get data client expired");
+        resolve(result.data);
+      })
+      .catch((err) => {
+        console.log("gagal get data client expired");
+        reject(err);
+      });
+  });
+};
+
+export { getSumClient, getClient, getExpired };
