d6e464e86328b64f55af814c896b2653c54ed051
IwanHPrasetyo <Auxx28@gmail.com>
2023-01-23 01:17:19 +0700

    date expired range in day

--- src/app/screen/dashboard/index.js
+++ src/app/screen/dashboard/index.js
@@ -251,9 +251,7 @@ const Dashboard = () => {
           <div className="card">
             <div className="card-body">
               <div className="d-flex flex-row justify-content-between">
-                <h4 className="card-title mb-1">
-                  Pengguan Masa Aktif Akan Berakhir
-                </h4>
+                <h4 className="card-title mb-1">Masa Aktif Akan Berakhir</h4>
                 <p className="text-muted mb-1">Tanggal Masa Aktif</p>
               </div>
               <div className="row">
