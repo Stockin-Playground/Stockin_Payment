0d248f805e319c4113caca866b03fc65c3b847e7
IwanHPrasetyo <Auxx28@gmail.com>
2023-02-24 15:17:21 +0700

    route login dashborad

--- src/app/screen/user/Login.js
+++ src/app/screen/user/Login.js
@@ -1,50 +1,83 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Form } from 'react-bootstrap';
+import React, { Component, useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
 
-export class Login extends Component {
-  render() {
-    return (
-      <div>
-        <div className="d-flex align-items-center auth px-0">
-          <div className="row w-100 mx-0">
-            <div className="col-lg-4 mx-auto">
-              <div className="card text-left py-5 px-4 px-sm-5">
-                <div className="brand-logo">
-                 <img src="/static/media/stockin_landscape.05de50b2.png" alt="logo"/>
-                </div>
-                <h4>Stockin Invoice Login</h4>
-                <h6 className="font-weight-light">Sign in to continue.</h6>
-                <Form className="pt-3">
-                  <Form.Group className="d-flex search-field">
-                    <Form.Control type="email" placeholder="Email" size="lg" className="h-auto" />
-                  </Form.Group>
-                  <Form.Group className="d-flex search-field">
-                    <Form.Control type="password" placeholder="Password" size="lg" className="h-auto" />
-                  </Form.Group>
-                  <div className="mt-3">
-                    <Link className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" to="/dashboard">SIGN IN</Link>
-                  </div>
-                  <div className="my-2 d-flex justify-content-between align-items-center">
-                    <div className="form-check">
-                      <label className="form-check-label text-muted">
-                        <input type="checkbox" className="form-check-input"/>
-                        <i className="input-helper"></i>
-                        Keep me signed in
-                      </label>
-                    </div>
-                    <a href="getLogin" onClick={event => event.stopPropagation()} className="auth-link text-muted">Forgot password?</a>
-                  </div>
-                 
-                  
-                </Form>
+const Login = () => {
+  const history = useHistory();
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.log("jalan e");
+    const formData = new FormData(e.target),
+      formDataObj = Object.fromEntries(formData.entries());
+    console.log(formDataObj);
+
+    onDetailClient();
+  };
+
+  const onDetailClient = useCallback(
+    () => history.push("/dashboard"),
+    [history]
+  );
+
+  return (
+    <div>
+      <div className="d-flex align-items-center auth px-0">
+        <div className="row w-100 mx-0">
+          <div className="col-lg-4 mx-auto">
+            <div className="card text-left py-5 px-4 px-sm-5">
+              <div className="brand-logo">
+                <img
+                  src="/static/media/stockin_landscape.05de50b2.png"
+                  alt="logo"
+                />
               </div>
+              <h4>Stockin Invoice Login</h4>
+              <h6 className="font-weight-light">Sign in to continue.</h6>
+              <Form onSubmit={onFormSubmit} className="pt-3">
+                <Form.Group className="d-flex search-field">
+                  <Form.Control
+                    type="email"
+                    placeholder="Email"
+                    size="lg"
+                    className="h-auto"
+                    name="email"
+                  />
+                </Form.Group>
+                <Form.Group className="d-flex search-field">
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    size="lg"
+                    className="h-auto"
+                    name="password"
+                  />
+                </Form.Group>
+                <div className="mt-3">
+                  <Button
+                    className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"
+                    type="submit"
+                  >
+                    SIGN IN
+                  </Button>
+                </div>
+                <div className="my-2 d-flex justify-content-between align-items-center">
+                  <a
+                    href="getLogin"
+                    onClick={(event) => event.stopPropagation()}
+                    className="auth-link text-muted"
+                  >
+                    Lupa password ?
+                  </a>
+                </div>
+              </Form>
             </div>
           </div>
-        </div>  
+        </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
